19099fbf8e2fa456a88e498dbb924f8fbc9a4140
petrodmytriv <petro.dmytriv@lembersolutions.com>
2022-02-21 11:19:34 +0200

    feat: add ship cargo calculation

---
+++ src/constants/interface.ts
@@ -0,0 +1,7 @@
+export interface ShipmentInterface {
+  id: string;
+  name: string;
+  email: string;
+  route: string;
+  boxes: string;
+}
--- src/pages/CargoCalculation/CargoCalculation.tsx
+++ src/pages/CargoCalculation/CargoCalculation.tsx
@@ -3,6 +3,7 @@ import shipment from "../../query/shipments.json";
 import { useEffect, useState } from "react";
 
 export const CargoCalculation = () => {
+  let data: any;
   const [cargo, setCargo] = useState("");
   const [bays, setBays] = useState(0);
 
@@ -13,22 +14,46 @@ export const CargoCalculation = () => {
     return setBays(Math.ceil(sum / 10));
   }, [cargo]);
 
+  useEffect(() => {
+    setCargo(data.boxes);
+  }, []);
+
+  const path = window.location.pathname;
+
+  const checkPath = () => {
+    switch (path) {
+      case "/amazon":
+        return (data = shipment[0]);
+      case "/american_express":
+        return (data = shipment[1]);
+      case "/airbnb":
+        return (data = shipment[2]);
+      case "/apple":
+        return (data = shipment[3]);
+    }
+  };
+  checkPath();
   return (
     <article className={styles.wrapper}>
       <div className={styles.content}>
-        <h2 className={styles.name}>{shipment[0].name}</h2>
-        <p className={styles.email}>{shipment[0].email}</p>
+        <h2 className={styles.name}>{data.name}</h2>
+        <p className={styles.email}>{data.email}</p>
         <div className={styles.cargo}>
           <p className={styles.inputTitle}>cargo boxes</p>
           <input
             className={styles.input}
             type="text"
-            onChange={(e) => setCargo(e.target.value)}
+            value={cargo}
+            onChange={(e) => {
+              setCargo(e.target.value);
+            }}
           />
         </div>
         <div>
-          <p className={styles.required}>Number of required cargo bays</p>{" "}
-          <p className={styles.bays}>{bays}</p>
+          <p className={styles.required}>Number of required cargo bays</p>
+          <p className={styles.bays}>
+            {bays ? bays : "Please enter a valid number"}
+          </p>
         </div>
       </div>
     </article>
