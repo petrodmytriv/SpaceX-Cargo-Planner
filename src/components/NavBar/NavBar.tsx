e88590887f1779c16c99b7ed1d065503e7f47ed5
petrodmytriv <petro.dmytriv@lembersolutions.com>
2022-02-23 10:48:37 +0200

    fix: findDomNode error fixed

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import shipment from "../../query/shipments.json";
 import styles from "./NavBar.module.scss";
 import burger from "../../assets/icons/Burger.svg";
 import cancel from "../../assets/icons/Cancel.svg";
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useState, useRef } from "react";
 import { CSSTransition } from "react-transition-group";
 import "../../styles/animation.scss";
 import { useParams } from "react-router-dom";
@@ -27,21 +27,26 @@ export const NavBar: FunctionComponent = () => {
     );
   });
 
+  const menuRef = useRef(null)
+  const burgerRef = useRef(null)
+
   return (
     <>
       <CSSTransition
+          nodeRef={menuRef}
         in={showMenu}
         timeout={400}
         classNames='menu'
         onEnter={() => setShowMenu(true)}
         onExited={() => setShowMenu(false)}
       >
-        <nav className={!showMenu ? styles.menu : styles.mobileMenu}>
+        <nav ref={menuRef} className={!showMenu ? styles.menu : styles.mobileMenu}>
           <h4 className={styles.title}>shipment list</h4>
           <ul className={styles.list}>{menuNames}</ul>
         </nav>
       </CSSTransition>
       <CSSTransition
+          nodeRef={burgerRef}
         in={showMenu}
         timeout={400}
         classNames='burger'
@@ -49,6 +54,7 @@ export const NavBar: FunctionComponent = () => {
         onExited={() => setShowMenu(false)}
       >
         <button
+            ref={burgerRef}
           className={styles.burger}
           onClick={() => {
             setShowMenu(!showMenu);
